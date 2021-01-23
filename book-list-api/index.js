fa81d869e17b07be19e22126225cbcf3d0e7856f
levilu800b <levilu800b@gmail.com>
2021-01-23 12:38:52 +0000

    jet

--- book-list-api/index.js
+++ book-list-api/index.js
@@ -7,4 +7,4 @@ app.use(express.json());
 app.use(router)
 
 app.listen(port, () => 
-console.log(`Example app listening at https://localhost:${port}`))
+console.log(`Example app listening at https://booklist-api-express.herokuapp.com/:${port}`))
